5f439978c5cf5c520d5c9b354a172c0d2d2084ff
Lucian <lucian.mathes@tum.de>
2025-01-09 16:04:02 +0100

    add missing import to __init__

--- src/limpid/__init__.py
+++ src/limpid/__init__.py
@@ -1,7 +1,7 @@
 from .limpid import Layer, Sample
 from .visualize import (
-    check_sample_status, initial_guess, initial_guess, detailed_fit_result,
-    plot_fractions
+    check_sample_status, initial_guess, initial_guess, fit_result,
+    detailed_fit_result, plot_fractions
 )
 from .example_data import load_example_data, list_example_data
 
